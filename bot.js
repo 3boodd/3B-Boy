9fb4840a85540e0efe8e209fda2da39875b5879b
3boodd <36028899+3boodd@users.noreply.github.com>
2018-07-21 00:57:28 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,21 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-
-  let channel = ["469575636432846870"];
 client.on('voiceStateUpdate', (Codes, ReBeL) => {
-client.channels.get(channel);
-    ReBeL.guild.member(ReBeL).addRole(ReBeL.guild.roles.find("name", "ولد"))
-                  ReBeL.guild.createChannel('3B', 'voice').then(c => {
-                ReBeL.setVoiceChannel(c).then(() => {
-                    c.delete(305).catch(console.log);
-  console.log("Done");
+    let channel3 = ["469575636432846870"];
+client.channels.get(channel3)
+ReBeL.guild.member(ReBeL).addRole(ReBeL.guild.roles.find("name", "ولد")).then(r => {
+      });
+console.log("Done");
+});
+  client.on('voiceStateUpdate', (codes, ReBeL) => {
+if(ReBeL.voiceChannelID !== "469575636432846870") return console.log("أيرور . ");
+ReBeL.guild.createChannel(ReBeL.user.username , 'voice').then((rebeeel) =>{
+ReBeL.guild.members.get(ReBeL.id).setVoiceChannel(rebeeel.id).then((codess) =>{
+  console.log("تــــــم .");
+  let scan = setInterval(()=>{
+  rebeeel.delete();
+  }, 305);
 });
 });
 });
