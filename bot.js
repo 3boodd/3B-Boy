727d2a137af1cbc39b8abb74697226d1767301f7
3boodd <36028899+3boodd@users.noreply.github.com>
2018-07-20 05:30:56 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,8 @@
-  let channel = ["أيدي الروم الصوتي ."];
+  let channel = ["469575636432846870"];
 client.on('voiceStateUpdate', (Codes, ReBeL) => {
 client.channels.get(channel);
-    ReBeL.guild.member(ReBeL).addRole(ReBeL.guild.roles.find("name", "أسم الرتبه حقت التفعيل"))
-                  ReBeL.guild.createChannel('ReBeL', 'voice').then(c => {
+    ReBeL.guild.member(ReBeL).addRole(ReBeL.guild.roles.find("name", "ولد"))
+                  ReBeL.guild.createChannel('3B', 'voice').then(c => {
                 ReBeL.setVoiceChannel(c).then(() => {
                     c.delete(305).catch(console.log);
   console.log("Done");
