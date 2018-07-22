3cdd6f0bd6ffa5d64448f2c1c84b296ec04207a8
3boodd <36028899+3boodd@users.noreply.github.com>
2018-07-22 18:21:44 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,26 +23,5 @@ ReBeL.guild.members.get(ReBeL.id).setVoiceChannel(rebeeel.id).then((codess) =>{
 
 
 
-client.on('voiceStateUpdate', (Codes, ReBeL) => {
-    let channel3 = ["469575710558650388"];
-client.channels.get(channel3)
-ReBeL.guild.member(ReBeL).addRole(ReBeL.guild.roles.find("name", "بنت")).then(r => {
-      });
-console.log("Done");
-});
-  client.on('voiceStateUpdate', (codes, ReBeL) => {
-if(ReBeL.voiceChannelID !== "469575710558650388") return console.log("أيرور . ");
-ReBeL.guild.createChannel(ReBeL.user.username , 'voice').then((rebeeel) =>{
-ReBeL.guild.members.get(ReBeL.id).setVoiceChannel(rebeeel.id).then((codess) =>{
-  console.log("تــــــم .");
-  let scan = setInterval(()=>{
-  rebeeel.delete();
-  }, 305);
-});
-});
-});
-
-
-
 
 client.login(process.env.BOT_TOKEN);  //اياكككك تلعب هنا لا تحط توكنك هنا 
