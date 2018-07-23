56072b4f870402da240f70f243c15e90f8ac0268
3boodd <36028899+3boodd@users.noreply.github.com>
2018-07-23 12:31:40 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const fs = require("fs");
 
 let bane = JSON.parse(fs.readFileSync("./bcer.json", "utf8"));
 let banse = new Set();
